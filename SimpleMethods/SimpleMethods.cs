834783f147179e5ba21f8bdf6b6c58fb2c748f8c
Bret Hayes <bret_hayes@comcast.net>
2019-03-04 13:38:57 -0500

    Added the input question method and the open menu choice selection.

---
+++ SimpleMethods/SimpleMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethods
+{
+    public class SimpleScanMethods
+    {
+        public string taskInput(string task)
+        {
+            // task input method
+            Console.WriteLine("What task do you have have that needs to be done?");
+            task = Console.ReadLine();
+            return task;
+        }
+
+        public char choiceMenu(char select)
+        {
+            //Main menu selection of add, modify, or delete
+            Console.WriteLine("Would you like to:");
+            Console.WriteLine("[A] - Create New\n\t[B] - Modify Existing\n");
+            select = char.Parse(Console.ReadLine());
+            return select;
+        }
+    }
+}
--- SimpleScanningAssignment/SimpleScanning.cs
+++ SimpleScanningAssignment/SimpleScanning.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleMethods;
 
 namespace SimpleScanningAssignment
 {
-    class Program
+    class SimpleScanning
     {
         static void Main(string[] args)
         {
+            SimpleScanMethods simpleScan = new SimpleScanMethods();
+            // Select a task, next page, add a task, or delete
             // Input Tasks
             // Choose task, going down the list
             // re-enter/ cross-off (grey out)
             // Save in file
             // 25 lines to a page, including greyed out items
-            // Select a task, next page, add a task, or delete
        
         }
     }
